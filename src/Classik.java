7583ae0a11411784d294e506c30ff27ee9c9f249
Stepan <121403794+StepanKoz@users.noreply.github.com>
2023-08-26 15:10:11 +0400

    commit

--- src/Classik.java
+++ src/Classik.java
@@ -1,3 +1,16 @@
 public class Classik {
     private int counter;
+    public void count(int val){counter += val;}
+    public Classik(int counter){this.counter=counter;}
+
+    public int getCounter() {
+        return counter;
+
+
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    Classik cl = new Classik(0);
+    cl.count()
 }
 
 
