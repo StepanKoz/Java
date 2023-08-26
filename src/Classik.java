eca0504d1915180e8769fe5e3b8ac627b3e5797f
Stepan <121403794+StepanKoz@users.noreply.github.com>
2023-08-26 14:55:44 +0400

    commit

--- src/Classik.java
+++ src/Classik.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class Classik {
+public class Classik {
+    private int counter;
 }
--- src/Second.java
+++ src/Second.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class Second {
+public class Second {
+     public String name;
 }
