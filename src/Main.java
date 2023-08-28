ed458f5780e07e2f68e052db14fbf2e0617f9a63
Stepan <121403794+StepanKoz@users.noreply.github.com>
2023-08-28 22:00:25 +0400

    калькулятор

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
-import shop.Shop;
-public class Main {
-    public static void main(String[] args){
-        String myshop="Ebay";
-        Shop shop1=new Shop(myshop);
-    }
-//    Classik cl = new Classik(0);
-//    cl.count()
 
-}
+public class Main {
+    public static void main(String[] args) {
+
+        Classik cl = new Classik(0);
+        cl.count(0);
+    }
+    }
+
+
 
 
